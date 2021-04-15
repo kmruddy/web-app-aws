e0a5d5e9d4e122f2f554b9e347132a49191b5c26
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-04-15 16:56:25 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-west-2"
 }
+
+variable "bucket_name" {
+  descrtiption = "AWS S3 Bucket Name."
+  default = "bucket name"
+  }
